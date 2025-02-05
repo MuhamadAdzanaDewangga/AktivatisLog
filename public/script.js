73c0556adc6de1af7deb5bf9151129abffb6f9a1
MuhamadAdzanaDewangga <55965934+MuhamadAdzanaDewangga@users.noreply.github.com>
2025-02-05 16:00:27 +0700

    Delete public directory

--- public/script.js
+++
@@ -1,9 +0,0 @@
-fetch("https://your-railway-backend-url.onrender.com")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("status").innerText = data;
-    })
-    .catch(error => {
-        document.getElementById("status").innerText = "Error fetching API";
-        console.error(error);
-    });
